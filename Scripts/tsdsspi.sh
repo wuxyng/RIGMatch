625f16e8edf4b3783bf3c78a1b21669e02d9b8f4
wuxyng <xiaoying.wu@gmail.com>
2022-09-27 00:58:55 +0800

    deletion

--- Scripts/tsdsspi.sh
+++
@@ -1,5 +0,0 @@
-cd /home/work/TwigStackDSSPI
-export CLASSPATH=./bin:/home/work/JARs/commons-math3-3.6.jar:/home/JARs/algs4.jar:/home/work/JARs/dtdparser121.jar:/home/work/JARs/java_cup_11.jar
-
-java graph.main.TwigStackDMain ep_lb20.gra inst_lb20_tree_m.qry
-
